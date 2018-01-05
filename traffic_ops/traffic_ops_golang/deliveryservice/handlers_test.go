8c7d45993d8285e5fbb25ebcd7cb8d5f85e88aec
Dewayne Richardson <dewrich@apache.org>
2018-01-05 14:26:49 -0700

    created the initial version of the deliveryservices handler

--- traffic_ops/traffic_ops_golang/deliveryservice/handlers_test.go
+++ traffic_ops/traffic_ops_golang/deliveryservice/handlers_test.go
@@ -22,96 +22,45 @@ package deliveryservice
 import (
 	"encoding/json"
 	"fmt"
-	"sort"
-	"strings"
+	"net/url"
 	"testing"
 
-	"github.com/apache/incubator-trafficcontrol/traffic_ops/traffic_ops_golang/utils"
+	tcapi "github.com/apache/incubator-trafficcontrol/lib/go-tc/v13"
+	"github.com/apache/incubator-trafficcontrol/traffic_ops/traffic_ops_golang/test"
+	"github.com/jmoiron/sqlx"
+
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-// TestValidateErrors ...
-func TestValidateErrors(t *testing.T) {
-
-	ds := GetRefType()
-	if err := json.Unmarshal([]byte(errorTestCase()), &ds); err != nil {
-		fmt.Printf("err ---> %v\n", err)
-		return
-	}
-
-	errors := ds.Validate(nil)
-	errorStrs := utils.ErrorsToStrings(errors)
-	sort.Strings(errorStrs)
-	errorsFmt, _ := json.MarshalIndent(errorStrs, "", "  ")
-
-	expected := []string{
-		"'active' is required",
-		"'cdnId' is required",
-		"'geoLimit' is required",
-		"'geoProvider' is required",
-		"'infoUrl' must be a valid URL",
-		"'initialDispersion' must be greater than zero",
-		"'logsEnabled' is required",
-		"'orgServerFqdn' must be a valid URL",
-		"'routingName' cannot contain periods",
-		"'routingName' the length must be between 1 and 48",
-		"'typeId' is required",
-		"'xmlId' cannot contain spaces",
-		"'xmlId' is required",
-		"'xmlId' the length must be between 1 and 48",
-	}
-	sort.Strings(expected)
-	expectedFmt, _ := json.MarshalIndent(expected, "", "  ")
-
-	for _, e := range errorStrs {
-		if !findNeedle(e, expected) {
-			t.Errorf("\nExpected %s \n Actual %v", string(expectedFmt), string(errorsFmt))
-			break
-		}
-	}
-
-}
-
-func findNeedle(needle string, haystack []string) bool {
-	found := false
-	for _, s := range haystack {
-		if s == needle {
-			found = true
-			break
-		}
-		//fmt.Printf("(%t) Comparing [%v] with [%v]\n", found, exp, et)
-	}
-	return found
-}
-
-func errorTestCase() string {
-
-	routingName := strings.Repeat("X", 1) + "." + strings.Repeat("X", 48)
-
-	// Test the xmlId length
-	xmlId := strings.Repeat("X", 1) + " " + strings.Repeat("X", 48)
-
-	displayName := strings.Repeat("X", 49)
-
-	errorTestCase := `
+func getTestDeliveryServices() []tcapi.DeliveryService {
+	//Initial test fixture
+	const testCase = `
+[
 {
+   "active": true,
    "ccrDnsTtl": 1,
-   "checkPath": "/crossdomain.xml",
-   "displayName": "` + displayName + `",
+   "cdnId": 1,
+   "checkPath": "disp1",
+   "displayName": "/crossdomain.xml",
    "dnsBypassCname": "cname",
    "dnsBypassIp": "127.0.0.1",
    "dnsBypassIp6": "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
    "dnsBypassTTL": 10,
    "dscp": 0,
    "edgeHeaderRewrite": "cond %{REMAP_PSEUDO_HOOK} __RETURN__ set-config proxy.config.http.transaction_active_timeout_in 10800 [L]",
+   "geoLimit": 0,
    "geoLimitCountries": "Can,Mex",
    "geoRedirectURL": "http://localhost/redirect",
+   "geoProvider": 0,
    "globalMaxMBPS": 0,
    "globalMaxTPS": 0,
    "httpBypassFqdn": "http://bypass",
    "id": 1,
+   "infoUrl": "http://info.url",
    "initialDispersion": 0,
-   "infoUrl": "htt://info.url",
+   "ipv6RoutingEnabled": false,
    "lastUpdated": "2017-01-05 15:04:05+00",
+   "logsEnabled": true,
    "longDesc": "longdesc",
    "longDesc1": "longdesc1",
    "longDesc2": "longdesc2",
@@ -121,7 +70,7 @@ func errorTestCase() string {
    "missLong": -1.0,
    "multiSiteOrigin": false,
    "multiSiteOriginAlgorithm": 1,
-   "orgServerFqdn": "htt://localhost",
+   "orgServerFqdn": "http://localhost",
    "profile": 1,
    "protocol": 2,
    "qstringIgnore": 1,
@@ -129,14 +78,118 @@ func errorTestCase() string {
    "regexRemap": "^/([^\/]+)/(.*) http://$1.foo.com/$2",
    "regionalGeoBlocking": false,
    "remapText": "@action=allow @src_ip=127.0.0.1-127.0.0.1",
-   "routingName": "` + routingName + `",
+   "routineName": "ccr",
    "signingAlgorithm": "url_sig",
    "sslKeyVersion": 1,
    "tenantId": 1,
    "trRequestHeaders": "xyz",
    "trResponseHeaders": "Access-Control-Allow-Origin: *",
-   "xmlId": "` + xmlId + `"
- }
+   "typeId": 1,
+   "xmlId": "ds1"
+}
+]
 `
-	return errorTestCase
+	var dses []tcapi.DeliveryService
+	if err := json.Unmarshal([]byte(testCase), &dses); err != nil {
+		fmt.Printf("err ---> %v\n", err)
+		return nil
+	}
+
+	return dses
+}
+
+func TestGetDeliveryServices(t *testing.T) {
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	db := sqlx.NewDb(mockDB, "sqlmock")
+	defer db.Close()
+
+	cols := test.ColsFromStructByTag("db", tcapi.DeliveryService{})
+	rows := sqlmock.NewRows(cols)
+
+	for _, ts := range getTestDeliveryServices() {
+		rows = rows.AddRow(
+			ts.Active,
+			ts.CacheURL,
+			ts.CCRDNSTTL,
+			ts.CDNID,
+			ts.CheckPath,
+			ts.DisplayName,
+			ts.DNSBypassCNAME,
+			ts.DNSBypassIP,
+			ts.DNSBypassIP6,
+			ts.DNSBypassTTL,
+			ts.DSCP,
+			ts.EdgeHeaderRewrite,
+			ts.GeoLimit,
+			ts.GeoLimitCountries,
+			ts.GeoLimitRedirectURL,
+			ts.GeoProvider,
+			ts.GlobalMaxMBPS,
+			ts.GlobalMaxTPS,
+			ts.HTTPBypassFQDN,
+			ts.ID,
+			ts.InfoURL,
+			ts.InitialDispersion,
+			ts.IPV6RoutingEnabled,
+			ts.LastUpdated,
+			ts.LogsEnabled,
+			ts.LongDesc,
+			ts.LongDesc1,
+			ts.LongDesc2,
+			ts.MaxDNSAnswers,
+			ts.MidHeaderRewrite,
+			ts.MissLat,
+			ts.MissLong,
+			ts.MultiSiteOrigin,
+			ts.MultiSiteOriginAlgorithm,
+			ts.OrgServerFQDN,
+			ts.ProfileID,
+			ts.Protocol,
+			ts.QStringIgnore,
+			ts.RangeRequestHandling,
+			ts.RegexRemap,
+			ts.RegionalGeoBlocking,
+			ts.RemapText,
+			ts.RoutingName,
+			ts.SigningAlgorithm,
+			ts.SSLKeyVersion,
+			ts.TRRequestHeaders,
+			ts.TRResponseHeaders,
+			ts.TenantID,
+			ts.TypeID,
+			ts.XMLID,
+		)
+	}
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	v := url.Values{}
+	v.Set("xmlId", "1")
+
+	servers, err := getDeliveryServices(v, db)
+	if err != nil {
+		t.Errorf("getDeliveryServices expected: nil error, actual: %v", err)
+	}
+
+	if len(servers) != 2 {
+		t.Errorf("getDeliveryServices expected: len(servers) == 1, actual: %v", len(servers))
+	}
+
+}
+
+type SortableDeliveryServices []tcapi.DeliveryService
+
+func (s SortableDeliveryServices) Len() int {
+	return len(s)
+}
+func (s SortableDeliveryServices) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s SortableDeliveryServices) Less(i, j int) bool {
+	a := *s[i].XMLID
+	b := *s[j].XMLID
+	return a < b
 }
